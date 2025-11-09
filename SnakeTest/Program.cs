69f0c09d489c71fd1e9fc56b32d0299b1947fb3c
slefan991 <slefan991@gmail.com>
2025-11-09 18:44:45 +0100

    Fixed Movement bug.

--- SnakeTest/Program.cs
+++ SnakeTest/Program.cs
@@ -1,47 +1,67 @@
-﻿int snakeHeadPosY = 3; // Y = Up/Down
-int snakeHeadPosX = 0; // X = Left/Right
+﻿List<int> snakeTrackY = new List<int> { };
+List<int> snakeTrackX = new List<int> { };
+
+int snakeHeadPosY = 3; // Y = Up/Down
+int snakeHeadPosX = 1; // X = Left/Right
+
+int scoreBoxY = 0;
+int scoreBoxX = 0;
+
+int borderBoxY = 3;
+int borderBoxX = 0;
 bool keyPressed = false;
 
 int speedMs = 200;
 
-
 // axis 1 = -x, axis 2 = +x, axis 3 = -y, axis 4 = +y
 int axis = 2;
 
-string snakeBody = "#";
+string snakeBody = "▓";
 string snakeFood = "@";
+string borderChar = "║";
 int availableFood = 0;
 
 int foodLocationX = 0;
 int foodLocationY = 0;
 int snakeLength = 1;
+int score = snakeLength - 1;
 
 int windowWidth = Console.WindowWidth;
 int windowHeight = Console.WindowHeight;
 
 Console.CursorVisible = false;
 
-
-
-
 while (true)
 {
-    //Collision box
+    //Die when you hit the wall
     if (snakeHeadPosX == windowWidth - 1 || snakeHeadPosY == windowHeight - 1 || snakeHeadPosY < 0 || snakeHeadPosX < 0)
     {
-        Console.Clear();
         Console.Write("You lost!");
         break;
     }
 
-    if(availableFood == 0)
+    if(borderBoxX < windowWidth)
+    {
+        //Not working as intended, fixing later
+        //Draw borders
+
+        //Console.SetCursorPosition(borderBoxX, borderBoxY);
+        //Console.Write("║");
+        //borderBoxY++;
+    }
+
+    //Draw Scoreboard, add better UI later
+    Console.SetCursorPosition(1, 1);
+    Console.Write($"Score: {snakeLength - 1}");
+
+
+    if (availableFood == 0)
     {
         Random rand = new Random();
         foodLocationX = rand.Next(0, windowWidth);
         foodLocationY = rand.Next(0, windowHeight);
 
         availableFood++;
-
     }
 
     if(snakeHeadPosX == foodLocationX && snakeHeadPosY == foodLocationY)
@@ -51,16 +71,28 @@ while (true)
     }
 
     
-
     //Debug Position
-    Console.Write($"Position X: {snakeHeadPosX}; Position Y: {snakeHeadPosY}\n");
-    Console.Write($"Window Width: {windowWidth}; Window height: {windowHeight}");
+    //Console.Write($"Position X: {snakeHeadPosX}; Position Y: {snakeHeadPosY}\n");
+    //Console.Write($"Window Width: {windowWidth}; Window height: {windowHeight}");
 
     Thread.Sleep(speedMs);
-    Console.Clear();
     Console.SetCursorPosition(foodLocationX, foodLocationY);
     Console.Write(snakeFood);
     Console.SetCursorPosition(snakeHeadPosX, snakeHeadPosY);
+
+    //Adds cordinates to list
+    snakeTrackX.Add(snakeHeadPosX);
+    snakeTrackY.Add(snakeHeadPosY);
+    
+    if(snakeTrackX.Count > snakeLength)
+    {
+        snakeTrackX.RemoveAt(0);
+        snakeTrackY.RemoveAt(0);
+        
+        Console.SetCursorPosition(snakeTrackX[0], snakeTrackY[0]);
+        Console.Write(" ");
+    }
+
     Console.Write(snakeBody);
 
 
@@ -69,19 +101,19 @@ while (true)
     {
         if (axis == 1)
         {
-            snakeHeadPosY--;
+            snakeHeadPosX--;
         }
         else if (axis == 2) 
         {
-            snakeHeadPosY++;
+            snakeHeadPosX++;
         }
         else if(axis == 3)
         {
-            snakeHeadPosX--;
+            snakeHeadPosY--;
         }
         else if(axis == 4)
         {
-            snakeHeadPosX++;
+            snakeHeadPosY++;
         }
     }
 
@@ -92,27 +124,26 @@ while (true)
         if (k.Key == ConsoleKey.S)
         {
             keyPressed = true;
-            axis = 2;
+            axis = 4;
             keyPressed = false;
             continue;
-
         }
         else if (k.Key == ConsoleKey.W)
-        {
-            keyPressed = true;
-            axis = 1;
-            keyPressed = false;
-        }
-        else if (k.Key == ConsoleKey.A)
         {
             keyPressed = true;
             axis = 3;
             keyPressed = false;
         }
+        else if (k.Key == ConsoleKey.A)
+        {
+            keyPressed = true;
+            axis = 1;
+            keyPressed = false;
+        }
         else if (k.Key == ConsoleKey.D)
         {
             keyPressed = true;
-            axis = 4;
+            axis = 2;
             keyPressed = false;
         }
     }
